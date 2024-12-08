923de773036cfec540cdbc82faa2fa07e5d3c677
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2024-12-08 21:57:31 +0100

    Servomoteur Alone mode + Rapport

---
+++ sof/Old_Version/telemetre_vlad/obj/default/telemetre_sept_seg.d
@@ -0,0 +1,19 @@
+obj/default/telemetre_sept_seg.o: telemetre_sept_seg.c \
+ C:/Users/vladi/Desktop/Elec/Architecture_SE/Radar_2D_Vlad/sof/telemetre_vlad_bsp/system.h \
+ C:/Users/vladi/Desktop/Elec/Architecture_SE/Radar_2D_Vlad/sof/telemetre_vlad_bsp/linker.h \
+ c:\users\vladi\desktop\elec\architecture_se\radar_2d_vlad\sof\telemetre_vlad_bsp\hal\inc\io.h \
+ c:\users\vladi\desktop\elec\architecture_se\radar_2d_vlad\sof\telemetre_vlad_bsp\hal\inc\alt_types.h \
+ c:\users\vladi\desktop\elec\architecture_se\radar_2d_vlad\sof\telemetre_vlad_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ C:/Users/vladi/Desktop/Elec/Architecture_SE/Radar_2D_Vlad/sof/telemetre_vlad_bsp/telemetre.h
+
+C:/Users/vladi/Desktop/Elec/Architecture_SE/Radar_2D_Vlad/sof/telemetre_vlad_bsp/system.h:
+
+C:/Users/vladi/Desktop/Elec/Architecture_SE/Radar_2D_Vlad/sof/telemetre_vlad_bsp/linker.h:
+
+c:\users\vladi\desktop\elec\architecture_se\radar_2d_vlad\sof\telemetre_vlad_bsp\hal\inc\io.h:
+
+c:\users\vladi\desktop\elec\architecture_se\radar_2d_vlad\sof\telemetre_vlad_bsp\hal\inc\alt_types.h:
+
+c:\users\vladi\desktop\elec\architecture_se\radar_2d_vlad\sof\telemetre_vlad_bsp\drivers\inc\altera_avalon_pio_regs.h:
+
+C:/Users/vladi/Desktop/Elec/Architecture_SE/Radar_2D_Vlad/sof/telemetre_vlad_bsp/telemetre.h:
---
+++ sof/telemetre_onze/obj/default/telemetre_sept_seg.d
@@ -0,0 +1,19 @@
+obj/default/telemetre_sept_seg.o: telemetre_sept_seg.c \
+ C:/Users/vladi/Desktop/software/telemetre_onze_bsp/system.h \
+ C:/Users/vladi/Desktop/software/telemetre_onze_bsp/linker.h \
+ c:\users\vladi\desktop\software\telemetre_onze_bsp\hal\inc\io.h \
+ c:\users\vladi\desktop\software\telemetre_onze_bsp\hal\inc\alt_types.h \
+ c:\users\vladi\desktop\software\telemetre_onze_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ C:/Users/vladi/Desktop/software/telemetre_onze_bsp/telemetre.h
+
+C:/Users/vladi/Desktop/software/telemetre_onze_bsp/system.h:
+
+C:/Users/vladi/Desktop/software/telemetre_onze_bsp/linker.h:
+
+c:\users\vladi\desktop\software\telemetre_onze_bsp\hal\inc\io.h:
+
+c:\users\vladi\desktop\software\telemetre_onze_bsp\hal\inc\alt_types.h:
+
+c:\users\vladi\desktop\software\telemetre_onze_bsp\drivers\inc\altera_avalon_pio_regs.h:
+
+C:/Users/vladi/Desktop/software/telemetre_onze_bsp/telemetre.h:
