d295cea5d37b3378345d54f030522678558aecea
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2024-12-15 05:18:57 +0100

    turning telemeter clk fixed v1

---
+++ sof/telemetre_sur_servomoteur_tourant_corrige_clk/software/telemetre_tournant_clk_corrigee/obj/default/turning_telemetre_clk_fixed.d
@@ -0,0 +1,19 @@
+obj/default/turning_telemetre_clk_fixed.o: turning_telemetre_clk_fixed.c \
+ ../telemetre_tournant_clk_corrigee_bsp/system.h \
+ ../telemetre_tournant_clk_corrigee_bsp/linker.h \
+ ../telemetre_tournant_clk_corrigee_bsp//HAL/inc/io.h \
+ ../telemetre_tournant_clk_corrigee_bsp//HAL/inc/alt_types.h \
+ ../telemetre_tournant_clk_corrigee_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../telemetre_tournant_clk_corrigee_bsp/turning_telemeter_clk_fixed.h
+
+../telemetre_tournant_clk_corrigee_bsp/system.h:
+
+../telemetre_tournant_clk_corrigee_bsp/linker.h:
+
+../telemetre_tournant_clk_corrigee_bsp//HAL/inc/io.h:
+
+../telemetre_tournant_clk_corrigee_bsp//HAL/inc/alt_types.h:
+
+../telemetre_tournant_clk_corrigee_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../telemetre_tournant_clk_corrigee_bsp/turning_telemeter_clk_fixed.h:
