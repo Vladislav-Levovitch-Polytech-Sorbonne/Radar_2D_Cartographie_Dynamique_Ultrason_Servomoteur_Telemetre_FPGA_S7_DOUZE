24de770bfd2ba98bbe8339ac146964167afda248
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2024-12-22 19:47:04 +0100

    Neopixel Merge with Radar Nios ii v0 - Motor + Preparatif Neopixel

---
+++ sof/Neopixel_v2/software/neopixel_three/obj/default/neopixel_radar.d
@@ -0,0 +1,18 @@
+obj/default/neopixel_radar.o: neopixel_radar.c \
+ ../neopixel_three_bsp/system.h ../neopixel_three_bsp/linker.h \
+ ../neopixel_three_bsp//HAL/inc/io.h \
+ ../neopixel_three_bsp//HAL/inc/alt_types.h \
+ ../neopixel_three_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../neopixel_three_bsp/neopixel_radar.h
+
+../neopixel_three_bsp/system.h:
+
+../neopixel_three_bsp/linker.h:
+
+../neopixel_three_bsp//HAL/inc/io.h:
+
+../neopixel_three_bsp//HAL/inc/alt_types.h:
+
+../neopixel_three_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../neopixel_three_bsp/neopixel_radar.h:
---
+++ sof/TEST_Neopixel/software/neopixel_three/obj/default/neopixel_alone.d
@@ -0,0 +1,18 @@
+obj/default/neopixel_alone.o: neopixel_alone.c \
+ ../neopixel_three_bsp/system.h ../neopixel_three_bsp/linker.h \
+ ../neopixel_three_bsp//HAL/inc/io.h \
+ ../neopixel_three_bsp//HAL/inc/alt_types.h \
+ ../neopixel_three_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../neopixel_three_bsp/neopixel.h
+
+../neopixel_three_bsp/system.h:
+
+../neopixel_three_bsp/linker.h:
+
+../neopixel_three_bsp//HAL/inc/io.h:
+
+../neopixel_three_bsp//HAL/inc/alt_types.h:
+
+../neopixel_three_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../neopixel_three_bsp/neopixel.h:
