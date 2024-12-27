e98ef516b0ccc865bca158d4d6145d67c0d9fc28
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2024-12-27 13:31:26 +0100

    UART_Neopixel_Servomotor_Telemeter Avalon - Correctif .h

--- sof/UART_NEOPIXEL_telemetre_tournant/software/UART_Neopixel_Telemetre_Servomoteur_Parametrable/obj/default/UART_Neopixel_Telemetre_Servomoteur_Parametrable.d
+++ sof/UART_NEOPIXEL_telemetre_tournant/software/UART_Neopixel_Telemetre_Servomoteur_Parametrable/obj/default/UART_Neopixel_Telemetre_Servomoteur_Parametrable.d
@@ -4,7 +4,8 @@ obj/default/UART_Neopixel_Telemetre_Servomoteur_Parametrable.o: \
  ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp/linker.h \
  ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp//HAL/inc/io.h \
  ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp//HAL/inc/alt_types.h \
- ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp/UART_Neopixel_Telemetre_Servomoteur.h
+ ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp/UART_Neopixel_Telemetre_Servomoteur.h \
+ ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp//drivers/inc/altera_avalon_pio_regs.h
 
 ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp/system.h:
 
@@ -15,3 +16,5 @@ obj/default/UART_Neopixel_Telemetre_Servomoteur_Parametrable.o: \
 ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp//HAL/inc/alt_types.h:
 
 ../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp/UART_Neopixel_Telemetre_Servomoteur.h:
+
+../UART_Neopixel_Telemetre_Servomoteur_Parametrable_bsp//drivers/inc/altera_avalon_pio_regs.h:
